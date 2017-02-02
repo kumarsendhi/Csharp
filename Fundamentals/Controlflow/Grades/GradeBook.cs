f36c9247587c4dc857ffb77bc28eabfcfaf2e329
Krishnamurthy <Kumar.Krishnamurthy@emc.com>
2017-02-02 15:42:50 +0530

    controlflow completed

---
+++ Fundamentals/Controlflow/Grades/GradeBook.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        List<float> grades;
+        private string _name;
+        public event NameChangedDelegate NameChanged;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                //if (!String.IsNullOrEmpty(value))
+                //{
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name should not be null");
+                }
+                if (_name != value)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.existingName = _name;
+                    args.newName = value;
+
+                    NameChanged(this, args);
+                }
+                _name = value;
+                //}
+            }
+        }
+        public GradeBook()
+        {
+            grades = new List<float>();
+            _name = "Empty";
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
+            }
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0;
+
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+
+            return stats;
+        }
+    }
+}
---
+++ Fundamentals/Controlflow/Grades/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            synth.Speak("Hello!, this is the grade book program");
+
+            GradeBook book = new GradeBook();
+
+           // book.NameChanged = new NameChangedDelegate(OnNameChanged);
+            //book.NameChanged = new NameChangedDelegate(OnNameChanged2);
+            book.NameChanged += new NameChangedDelegate(OnNameChanged);
+            // book.NameChanged += new NameChangedDelegate(OnNameChanged2);
+            //book.NameChanged = null;
+
+            
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+            //book.Name = "Kumar's Grade Book";
+            //book.Name = "Kumar' Grade Book";
+
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            book.WriteGrades(Console.Out);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            ////book.LowestGrade();
+
+            WriteResult("Average",stats.AverageGrade);
+            WriteResult("Highest Grade",(int)stats.HighestGrade);
+            WriteResult("Lowest Grade",stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+            Console.ReadLine();
+
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing from {args.existingName} to {args.newName}");
+        }
+
+        //static void OnNameChanged2(string existingName, string newName)
+        //{
+        //    Console.WriteLine($"****");
+        //}
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description+": "+result);
+        }
+
+
+    }
+}
